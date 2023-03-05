f623fdee7811b0295ff8dcbce8efd5d8b515fc53
ito <kira_neymar@icloud.com>
2023-03-06 01:08:07 +0900

    CORS設定を追加

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const app = express();
 const cors = require('cors');
 const Router = require('./routes/route')
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.headers.host) {
+        req.headers.host = 'accout-app-server.herokuapp.com'; // Replace with your own domain name
+    }
+    next();
+});
 app.use(express.json());
 app.use(cors());
 
